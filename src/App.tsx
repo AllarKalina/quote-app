565b1eeb0cc61ff2cee354e882cdba6775d2c68c
AllarKalina <55638689+AllarKalina@users.noreply.github.com>
2021-08-18 23:09:13 +0300

    Random quote from API

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,33 @@
-import React from 'react';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import { Quote } from './interfaces';
 
 function App() {
-  return <div></div>;
+  const [quote, setQuote] = useState<Quote | null>(null);
+  const url: string = 'https://api.quotable.io/random';
+
+  const fetchQuote = () => {
+    axios.get(url).then((res) => {
+      setQuote(res.data);
+    });
+  };
+
+  useEffect(() => {
+    fetchQuote();
+  }, []);
+
+  return (
+    <>
+      <header>
+        <h1>Quotes</h1>
+      </header>
+      <main>
+        <div>
+          <h2>{quote?.content}</h2>
+        </div>
+      </main>
+    </>
+  );
 }
 
 export default App;
